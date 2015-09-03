ae7018cec65de716eef77141b347526c047f9b7b
SAI MOHAN PEDDADA <GSPANN@SAIs-Mac-mini.local>
2015-09-03 15:28:38 +0530

    modified ipagen script

--- ipagen.sh
+++ ipagen.sh
@@ -1,9 +1,9 @@
 cd /Users/GSPANN/Desktop/VMAppWithKonylib/gen/
-perl extract.pl /Users/GSPANN/Jenkins/jobs/Ios_Build_CI/workspace/iphone/konyappiphone.KAR
+perl extract.pl /Users/GSPANN/Jenkins/jobs/GitHubiOSBuild/workspace/iphone/konyappiphone.KAR
 open ../VMAppWithKonylib.xcodeproj
 
 cd /Users/GSPANN/Desktop/VMAppWithKonylib
-xcodebuild -target "KRelease" -scheme "KRelease" -configuration Debug
+xcodebuild -target "KRelease" -scheme "KRelease" -configuration Release
 
 /usr/bin/xcrun -sdk iphoneos PackageApplication "/Users/GSPANN/Library/Developer/Xcode/DerivedData/VMAppWithKonylib-dximfjcrqiuxqdgiypafklnassdv/Build/Products/Debug-iphoneos/KonyIosApp.app" -o "/Users/GSPANN/Jenkins/jobs/GitHubiOSBuild/workspace/iphone/KonyIosApp.ipa"
 
